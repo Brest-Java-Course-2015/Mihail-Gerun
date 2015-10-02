d094fe46e291541a90e369d18f52b0313c586a54
Mikhail <hygo@mail.by>
2015-10-02 20:25:52 +0300

    second commit

---
+++ src/com/brest/epam/cources/Main.java
@@ -0,0 +1,13 @@
+package com.brest.epam.cources;
+/*e
+ * Created by mikhail on 02.10.15.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        int i =0;
+        System.out.println("test!");
+        i++;
+        System.out.println("test"+i);
+    }
+}
